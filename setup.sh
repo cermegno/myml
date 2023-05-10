2da4ef4422bd773b87aae8ddad1054cb5bbda46b
Alberto Ramos <alberto.ramos@dell.com>
2023-05-10 18:45:45 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 # Run git clone https://github.com/cermegno/myml
+# Change to the myml directory
 # then source setup.sh
 # It will copy the notebook and the data file to the right locations
-cp myml/ABC-sales.ipynb ml-env/
-cp myml/data/sales.csv ml-env/data/
+cp ABC-sales.ipynb ../ml-env/
+cp data/sales.csv  ../ml-env/data/
