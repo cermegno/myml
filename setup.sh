980f7dc8f667707407f61f72b99fc3b076d63272
Alberto Ramos <alberto.ramos@dell.com>
2023-05-11 11:32:55 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@
 # It will copy the notebook and the data file to the right locations
 cp ABC-sales.ipynb ../ml-env/
 cp data/sales.csv  ../ml-env/data/
-echo -e "You can start Jupyter browser like this ..."
+echo -e "=== Files have been copied to the right location ==="
+echo -e "=== You can start Jupyter browser as follows:    ==="
 echo -e "jupyter notebook --allow-root --ip=0.0.0.0 --port=80"
