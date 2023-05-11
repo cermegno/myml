6a15b4b4bf438b135f3b4c9cf828fc9304944819
Alberto Ramos <alberto.ramos@dell.com>
2023-05-11 11:17:34 +1000

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-# Run git clone https://github.com/cermegno/myml
-# Change to the myml directory
-# then source setup.sh
-# It will copy the notebook and the data file to the right locations
-cp ABC-sales.ipynb ../ml-env/
-cp data/sales.csv  ../ml-env/data/
