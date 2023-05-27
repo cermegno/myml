fc6f55fbbb337c9f1ceb617aa90115e1ccfa209e
Alberto Ramos <alberto.ramos@dell.com>
2023-05-27 10:15:04 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,12 @@
 # then source setup.sh
 # It will copy the notebook and the data file to the right locations
 cp ABC-sales.ipynb ../ml-env/
+mkdir ../ml-env/data/
 cp data/sales.csv  ../ml-env/data/
-echo -e "=== Files have been copied to the right location ==="
-echo -e "=== You can start Jupyter browser as follows:    ==="
+echo -e ""
+echo -e "===  File copy complete  ==="
+echo -e "=== Start Jupyter server ==="
+echo -e ""
 echo -e "jupyter notebook --allow-root --ip=0.0.0.0 --port=80"
+echo -e ""
+
